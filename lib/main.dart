b3484053556dec42ba6efddacb193b0637d1effa
Serkan Topal <serkan98topal@gmail.com>
2023-08-27 16:44:27 +0300

    firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:record/record.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
