0c9cd8ffbbbee9e1d598724991d9e10bacfd5446
Serkan Topal <serkan98topal@gmail.com>
2023-08-27 21:07:36 +0300

    no change

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:audioplayers/audioplayers.dart';
 
 import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
